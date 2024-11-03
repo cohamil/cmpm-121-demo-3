ecbb63ffaa8f59c0302415c4b9f67c1e6ccb30ad
cohamil <coohamil@ucsc.edu>
2024-11-02 21:50:46 -0700

    setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-// todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const appName = "Geocoin Carrier";
+document.title = appName;
+
+const header = document.createElement("h1");
+header.innerHTML = appName;
+app.append(header);
+
+const button = document.createElement("button");
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
+app.append(button);
